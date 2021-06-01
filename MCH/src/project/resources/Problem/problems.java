7def6f966094e557181bbedbc40e3c01a0d9e312
zhuaidadaya <78599649+zhuaidadaya@users.noreply.github.com>
2021-06-02 01:50:01 +0800

    Delete MCH directory

--- MCH/src/project/resources/Problem/problems.java
+++
@@ -1,8 +0,0 @@
-package project.resources.Problem;
-
-public class problems {
-    public static String math_p1_answer = "";
-    public static void setAnswer() {
-        math_p1_answer = "";
-    }
-}
